8e9ade46adfd83aec5d44fea91791def8157c9b0
onifaie <onifaie@gmail.com>
2022-06-17 19:43:02 +0300

    programming about gusst Number

---
+++ OOP2.py
@@ -0,0 +1,18 @@
+#This File about practecal  use class and function and looping 
+import random
+class RnadomNumber:
+    '''
+    this function ask user to Gusst Number Buttwen 0 to 20  if his chooes write print good lock 
+    anther wis  print Sorry your wrong Number Try agin !!!!!
+    '''
+    def Qusttnumber(self):
+        t=input("Enter your Number you Qusst ")
+        y=random.randint(0,20) 
+        if int(t)==y:
+            print ("good lock ")
+        else:
+            print("Sorry your wrong Number try agin ")
+        
+        
+c1=RnadomNumber()
+c1.Qusttnumber()
