febc90e763b3f7dc8246b00988fb37a779cd069d
onifaie <onifaie@gmail.com>
2022-06-18 21:38:47 +0300

    add json file

---
+++ json.py
@@ -0,0 +1,9 @@
+'''this file to read jsone file from website obeid.pro '''
+
+class GetAPI:
+    def getEmp(self):
+        print ("this my name is obeid come from website obeo.pro ")
+        
+        
+d=GetAPI()
+d.getEmp()
