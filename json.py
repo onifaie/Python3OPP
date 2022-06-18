b600c9cb9d699727b72c2b8d217afe69d0a76250
onifaie <onifaie@gmail.com>
2022-06-18 21:42:21 +0300

    sdd Folder

--- json.py
+++ json.py
@@ -3,7 +3,7 @@
 class GetAPI:
     def getEmp(self):
         print ("this my name is obeid come from website obeo.pro ")
-        
+        print("hello this my name test2 ")
         
 d=GetAPI()
 d.getEmp()
